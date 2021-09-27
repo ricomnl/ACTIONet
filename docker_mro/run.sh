be9b727b039879a5dd9fcb55500cfb7d6d240e88
Shahin Mohammadi <shahin.mohammadi@gmail.com>
2021-09-27 06:00:45 -0700

    Updated mro docker

--- docker_mro/run.sh
+++ docker_mro/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker run -it --rm -e USERID=1000 -e GROUPID=1000 -e UMASK=022 -p 8787:8787 -v `pwd`:/scratch shmohammadi86/actionet:mro
+docker run -it --rm -e USERID=1000 -e GROUPID=1000 -e UMASK=022 -p 8787:8787 -v `pwd`:/scratch actionet/actionet:mro
 
 
