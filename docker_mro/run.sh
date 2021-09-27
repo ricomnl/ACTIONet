c6c4c34980a0b3c36e8194bc10db140819500eed
Shahin Mohammadi <shahin.mohammadi@gmail.com>
2021-09-27 05:59:17 -0700

    Updated mro docker

--- docker_mro/run.sh
+++ docker_mro/run.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-export PARENT_DOCKER=shmohammadi86/actionet:mro
-export REGISTRY=shmohammadi86
-export IMAGE_NAME=actionet
-export VERSION=mro
-docker run -it --rm -e USERID=1000 -e GROUPID=1000 -e UMASK=022 -p 8787:8787 -v `pwd`:/scratch $(IMAGE_NAME):${VERSION}
+docker run -it --rm -e USERID=1000 -e GROUPID=1000 -e UMASK=022 -p 8787:8787 -v `pwd`:/scratch shmohammadi86/actionet:mro
 
 
