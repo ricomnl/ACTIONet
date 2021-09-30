e13ec87461a94e79aa57998bd178a9c5682d2c91
Shahin Mohammadi <shahin.mohammadi@gmail.com>
2021-09-30 07:43:35 -0700

    Removed dplot as dependencies and updated core submodule ref

--- docker_mro/run.sh
+++ docker_mro/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker run -it --rm -e USERID=1000 -e GROUPID=1000 -e UMASK=022 -p 8787:8787 -v `pwd`:/scratch actionet/actionet:mro
+docker run -it --rm -e USERID=`id -u` -e GROUPID=`id -g` -e UMASK=022 -e USER=actionet `pwd`:/home/actionet -p 8787:8787 -v actionet/actionet:mro
 
 
